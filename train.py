3df7793458123eaf8ab36291c98bb968bcea701b
SunDoge <384813529@qq.com>
2017-12-13 00:21:28 +0800

    fit;

--- train.py
+++ train.py
@@ -1,6 +1,26 @@
+import numpy as np
+from ensemble import AdaBoostClassifier
+from sklearn.ensemble import AdaBoostClassifier as SkAdaBoostClassifier
 
 
-if __name__ == "__main__":
-    # write your code here
-    pass
+def main():
+    clf = AdaBoostClassifier()
+    X_train = np.array([
+        [1, 1],
+        [1, 0],
+        [0, 1],
+        [0, 0]
+    ])
 
+    y_train = np.array([
+        0,
+        1,
+        1,
+        0
+    ])
+
+    clf.fit(X_train, y_train)
+
+
+if __name__ == '__main__':
+    main()

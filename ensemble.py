b2009a7c829bdd7abfe07dc6f17da90c7d119c3d
SunDoge <384813529@qq.com>
2017-12-18 18:45:35 +0800

    98%

--- ensemble.py
+++ ensemble.py
@@ -4,7 +4,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 
 
-
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
 
@@ -17,10 +16,14 @@ class AdaBoostClassifier:
         '''
         self.weak_classifier = weak_classifier
         self.n_weakers_limit = n_weakers_limit
+        self.learning_rate = 1.0
         self.le = LabelEncoder()
 
     def is_good_enough(self):
         '''Optional'''
+        pass
+
+    def _boost(self, X, y):
         pass
 
     def fit(self, X, y):
@@ -30,35 +33,51 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        n_features = X.shape[0]
+        n_samples = X.shape[0]
         # Label encode y
-        y = self.le.fit_transform(y)
-        epsilon = 0.1
+        # y = self.le.fit_transform(y)
+        self.le.fit(y)
+
+        self.n_classes = len(self.le.classes_)
+        epsilon = 0.01
         # clear weights
-        self.w = np.ones(n_features) / n_features
+        self.w = np.ones(n_samples) / n_samples
         self.models = []
         self.alphas = []
         # print(self.w)
         # max_depth = None
         for iboost in range(self.n_weakers_limit):
-            weak_classifier = self.weak_classifier()
-            weak_classifier.fit(X, y, sample_weight=self.w)
-            y_pred = weak_classifier.predict(X)
-            # print(iboost)
-            # print(y_pred)
 
-            error = self.w.dot(y_pred != y)
-            # print(error)
-            # error = 0.1 * self.w.min() if error == 0 else error
+            clf = self.weak_classifier(max_depth=1)
+            clf.fit(X, y, sample_weight=self.w)
+            y_pred = clf.predict(X)
 
-            alpha = 0.5 * np.log((1 - error + epsilon) / (error + epsilon))
-            # print(alpha)
+            incorrect = y_pred != y
 
-            self.w = self.w * np.exp(-alpha * y * y_pred)
-            self.w = self.w / self.w.sum()
+            error = np.mean(
+                np.average(incorrect, weights=self.w, axis=0))
 
-            self.models.append(weak_classifier)
+            # if error <= 0:
+            #     print(error)
+            #     alpha = 0
+            # else:
+
+            alpha = self.learning_rate * (
+                np.log((1. - error + epsilon) / (error + epsilon)) +
+                np.log(self.n_classes - 1.))
+
+            self.w *= np.exp(alpha * incorrect *
+                             ((self.w > 0) |
+                              (alpha < 0)))
+
+            self.models.append(clf)
             self.alphas.append(alpha)
+
+            # if error == 0:
+            #     break
+
+            self.w /= self.w.sum()
+            print("round %d/%d" % (iboost + 1, self.n_weakers_limit))
 
         return self
 
@@ -83,14 +102,25 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        y_pred = np.zeros(X.shape[0])
-        for alpha, clf in zip(self.alphas, self.models):
-            y_pred += alpha * clf.predict(X)
-            # print(alpha)
 
-        y_pred = np.sign(y_pred)
-        # print(y_pred)
-        return self.le.inverse_transform(y_pred.astype(int))
+        n_classes = self.n_classes
+        classes = self.le.classes_[:, np.newaxis]
+
+        pred = sum((clf.predict(X) == classes).T * alpha
+                   for clf, alpha in zip(self.models,
+                                         self.alphas))
+        # for clf, alpha in zip(self.models, self.alphas):
+        #     print(clf.predict(X) == classes).T * alpha
+        print(self.alphas)
+
+        pred /= sum(self.alphas)
+
+        if n_classes == 2:
+            pred[:, 0] *= -1
+            pred = pred.sum(axis=1)
+            return self.le.classes_.take(pred > 0, axis=0)
+
+        return self.le.classes_.take(np.argmax(pred, axis=1), axis=0)
 
     @staticmethod
     def save(model, filename):
@@ -101,33 +131,3 @@ class AdaBoostClassifier:
     def load(filename):
         with open(filename, "rb") as f:
             return pickle.load(f)
-
-
-if method == 'gd':            
-    grad = gradient(X_batch, y_batch, w)
-    w -= learning_rate * grad
-    
-elif method == 'NAG':
-    grad = gradient(X_batch, y_batch, w - gamma * v)
-    v = gamma * v + learning_rate * grad
-    w -= v
-    
-elif method == 'RMSProp':
-    grad = gradient(X_batch, y_batch, w)
-    G = gamma * G + (1 - gamma) * np.square(grad)
-    w -= learning_rate * 100 * grad / np.sqrt(G + epsilon)
-    
-elif method == 'AdaDelta':
-    grad = gradient(X_batch, y_batch, w)
-    G = gamma * G + (1 - gamma)* np.square(grad)
-    dw = -np.sqrt(delta + epsilon) / np.sqrt(G + epsilon) * grad
-    w += dw
-    delta = gamma * delta + (1 - gamma) * np.square(delta)
-    
-elif method == 'Adam':
-    t = i + 1
-    grad = gradient(X_batch, y_batch, w)
-    m = beta * m + (1 - beta) * grad
-    G = gamma * G + (1 - gamma) * np.square(grad)
-    alpha = learning_rate * 100 * np.sqrt(1 - gamma ** t) / (1 - beta ** t)
-    w -= alpha * m / np.sqrt(G + epsilon)
--- train.py
+++ train.py
@@ -2,37 +2,113 @@ import numpy as np
 from ensemble import AdaBoostClassifier
 from sklearn.ensemble import AdaBoostClassifier as SkAdaBoostClassifier
 from sklearn.datasets import load_breast_cancer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+from skimage import io, transform, util, color
+import matplotlib.pyplot as plt
+from feature import NPDFeature
+from joblib import Parallel, delayed
 
 
-def main():
+def test_xor():
+    X_train = np.array([
+        [1, 1],
+        [1, 0],
+        [0, 1],
+        [0, 0]
+    ])
+
+    y_train = np.array([
+        0,
+        1,
+        1,
+        0
+    ])
+
     clf = AdaBoostClassifier(n_weakers_limit=1000)
-    X, y = load_breast_cancer(True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    # print(X_train)
-    # print(y_train)
-    # X_train = np.array([
-    #     [1, 1],
-    #     [1, 0],
-    #     [0, 1],
-    #     [0, 0]
-    # ])
-
-    # y_train = np.array([
-    #     0,
-    #     1,
-    #     1,
-    #     0
-    # ])
 
     clf.fit(X_train, y_train)
-    y_pred = clf.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
+    y_pred = clf.predict(X_train)
+    print(classification_report(y_train, y_pred))
 
     skclf = SkAdaBoostClassifier()
     skclf.fit(X_train, y_train)
-    print(accuracy_score(y_test, skclf.predict(X_test)))
+    print(classification_report(y_train, skclf.predict(X_train)))
+
+
+def test_breast_cancer():
+    clf = AdaBoostClassifier(n_weakers_limit=50)
+    X, y = load_breast_cancer(True)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(classification_report(y_test, y_pred))
+
+    skclf = SkAdaBoostClassifier()
+    skclf.fit(X_train, y_train)
+    print(classification_report(y_test, skclf.predict(X_test)))
+
+
+def resize_image(img):
+    img = transform.resize(img, (24, 24))
+    img = color.rgb2gray(img)
+    img = util.img_as_ubyte(img)
+    return img
+
+
+def get_features(img):
+    img = resize_image(img)
+    features = NPDFeature(img).extract()
+    return features
+
+
+def test_image():
+    path = 'datasets/original/'
+    face = io.imread_collection(path + 'face/*.jpg')
+    nonface = io.imread_collection(path + 'nonface/*.jpg')
+    labels = ['face', 'nonface']
+
+    X = []
+    y = []
+
+    # face_list = [get_features(i) for i in face]
+    # nonface_list = [get_features(i) for i in nonface]
+    # face_list = Parallel(n_jobs=4)(delayed(get_features)(i) for i in face)
+    # nonface_list = Parallel(n_jobs=4)(
+    #     delayed(get_features)(i) for i in nonface)
+
+    # X += face_list
+    # y += list(np.zeros(len(face_list), dtype=int))
+    # X += nonface_list
+    # y += list(np.ones(len(nonface_list), dtype=int))
+
+    # AdaBoostClassifier.save(X, 'X.pkl')
+    # AdaBoostClassifier.save(y, 'y.pkl')
+    X = AdaBoostClassifier.load('X.pkl')
+    y = AdaBoostClassifier.load('y.pkl')
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        np.array(X), np.array(y), test_size=0.33, random_state=42)
+
+    print('start training')
+    clf = AdaBoostClassifier(n_weakers_limit=50)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(classification_report(y_test, y_pred, target_names=labels))
+
+    # skclf = SkAdaBoostClassifier(n_estimators=10)
+    # skclf.fit(X_train, y_train)
+    # print(classification_report(y_test, skclf.predict(X_test), target_names=labels))
+    # print(np.array(X))
+    # print(np.array(y))
+
+
+def main():
+    # test_xor()
+    # test_breast_cancer()
+    test_image()
 
 
 if __name__ == '__main__':
